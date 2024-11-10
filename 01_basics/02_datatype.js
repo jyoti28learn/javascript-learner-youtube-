47b855fd4fd68e71c1dd9c9add1bac46c761ba44
Jyoti <jyoti28learn@gmail.com>
2024-11-10 03:53:29 +0000

    02 datatypes lecture

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+//we are doing everything in node hence can't execute alert(3+3)
+
+//js ka engine browser k andr chipa hua h 
+//when to use ; ? =only for line maintain readabilty on ur own 
+
+//number 
+//bigint
+//string
+//bool
+
+//null :stanalone value, representation of an empty value like temperature = null 
+//undefined:not defined
+
+//symbol:for uniqueness
+
+//object
+
+let name = "maa"
+let age = 45
+let islogedIn = false
+
+console.log(typeof(name), typeof(age) , typeof(islogedIn)) //string number boolean
+
+console.log(typeof(null)) //object
+console.log(typeof(undefined)) //undefined
+
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # javascript-learner-youtube-
 a code repo for javascript learning from youtube
+
+
+documentation :
+js standard:ECMA standard js tc39.es
+mdn
