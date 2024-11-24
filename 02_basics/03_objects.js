c6d85c2b0fb25723bafd85904f708d137e3a3cdc
Jyoti <jyoti28learn@gmail.com>
2024-11-24 06:56:29 +0000

    03

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+/*ACCESS THE OBJECT USING . OPERATOR AND ALSO 
+this key value pairs of object the key is always the string 
+hence we can access it JsUser["email"]) this way also .
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])*/
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);  //string interpolation using backtiks
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
