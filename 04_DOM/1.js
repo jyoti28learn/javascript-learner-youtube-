d67805db463a3919bd954f09e08a0e7b9e485623
Jyoti <jyoti28learn@gmail.com>
2025-02-06 04:08:02 +0000

    unn

---
+++ 04_DOM/1.js
@@ -0,0 +1,5 @@
+//console.dir(document),console.log(window.document)
+//Document Object Model: flow: window->document->html->1.head(tile , meta)  -->2.body(div->(h1, p))
+//selector elements- getElementById , querySekctor and all , 
+//nodelist are not arrays :always use forEach(()=>{}) to access like array
+//to convert HTML collection to array: Array.from(value)
